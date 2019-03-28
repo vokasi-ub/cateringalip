71045aae52db02ffa480f425ace843a79e93c072
aliviasaskia <alivpuspa11@gmail.com>
2019-03-28 10:44:50 +0700

    migration project

---
+++ database/migrations/2019_03_27_033342_create_order.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->increments('id_order');
+            $table->string('atasnama');
+            $table->string('nohp');
+            $table->text('alamat');
+            $table->string('jumlah');
+            $table->date('tgl_order');
+            $table->datetime('tgl_ambil');
+            $table->string('harga_satuan');
+            $table->string('harga_total');
+            $table->string('statustransaksi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}

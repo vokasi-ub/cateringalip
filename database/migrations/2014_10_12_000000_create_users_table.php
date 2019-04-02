b01dd4aea29860e10772fc18f1bbabf39ca8aa54
aliviasaskia <alivpuspa11@gmail.com>
2019-04-02 21:42:41 +0700

    login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('idlogin');
-            $table->string('username');
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
